575973b1704af4627a142fd4f85b2d2e602c434d
Jason Farrell <imjason@gmail.com>
2022-02-18 22:13:26 -0500

    added env flag to prevent acr redeploy

--- sample3/main.tf
+++ sample3/main.tf
@@ -6,6 +6,7 @@ resource azurerm_resource_group rg {
 
 // create the container registry
 resource azurerm_container_registry acr {
+  count               = var.environment == "p" ? 0 : 1
   name                = "acr${var.app_name}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
