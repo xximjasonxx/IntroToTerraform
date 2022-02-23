39f223a89b82a563df9c30d86c8ba10ba4cfc3ad
Jason Farrell <imjason@gmail.com>
2022-02-22 21:12:16 -0500

    added command for rg create

--- sample1/main.tf
+++ sample1/main.tf
@@ -1,15 +1,14 @@
 
 // get a reference to an existing resource group (data source)
-resource azurerm_resource_group rg {
+data azurerm_resource_group rg {
   name      = "rg-terraform-sample1"
-  location  = "eastus"
 }
 
 // deploy a resource (storage account w/ container)
 resource azurerm_storage_account storage {
   name                      = "stterraformsample01"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  location                  = data.azurerm_resource_group.rg.location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
 }
