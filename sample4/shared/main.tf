fb5dd0fdaf3e5d78ad07465bd13b31eefbbb1c5c
Jason Farrell <imjason@gmail.com>
2022-04-03 09:52:17 -0400

    enabled admin credentials

--- sample4/shared/main.tf
+++ sample4/shared/main.tf
@@ -10,5 +10,5 @@ resource azurerm_container_registry acr {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 }
